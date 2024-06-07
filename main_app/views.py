e7caad88edd501ba8e380ba679b0e99232e64257
Echoesong <jayefelker@gmail.com>
2024-06-07 15:17:10 -0500

    add index pokemon route, view, template

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+# Views are equivalent to controllers in express?
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def pokemon_index(request):
+    pokemons = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+    return render(request, 'pokemon/index.html', { 'pokemons': pokemons})
