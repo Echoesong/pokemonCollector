57c3d076e00b85a9a0f968bac07db71d3ad10221
Echoesong <jayefelker@gmail.com>
2024-06-12 14:24:01 -0500

    try deploy fix

--- pokemoncollector/wsgi.py
+++ pokemoncollector/wsgi.py
@@ -8,6 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+try:
+    from dotenv import load_dotenv
+except ImportError as e:
+    sys.stderr.write(f"Error: {e}\n")
+    sys.exit(1)
+
+load_dotenv()
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemoncollector.settings')
+
+application = get_wsgi_application()
+
 
 from django.core.wsgi import get_wsgi_application
 
