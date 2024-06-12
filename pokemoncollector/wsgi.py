efe7fbc371ea1b8f11daba4264f5e09aa7ebef5a
Echoesong <jayefelker@gmail.com>
2024-06-12 13:17:51 -0500

    try good deploy fix

--- pokemoncollector/wsgi.py
+++ pokemoncollector/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-load_dotenv()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pokemoncollector.settings")
 
